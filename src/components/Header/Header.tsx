daa774ef41406ef450abd44e4e605b5e093d4796
Caio Lucas <clucaszrtmain@gmail.com>
2026-06-12 01:02:26 -0300

    chore: add HeaderProps interface

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,16 @@
 import { AiFillMoon, AiFillSun } from "react-icons/ai";
 
-export default function Header(props) {
+interface HeaderProps {
+  dark: boolean;
+  changeTheme: () => void;
+}
+
+export default function Header({ dark, changeTheme }: HeaderProps) {
   return (
     <header className="flex justify-between items-center px-5 py-3 dark:bg-black-base text-purple-primary border-b-primary-purple bg-off-white font-semibold text-xs border-b-2 dark:border-b-off-white duration-300">
       <p>@lucas-devsss</p>
-      <button onClick={() => props.changeTheme()} className="cursor-pointer">
-        {props.dark ? (
+      <button onClick={() => changeTheme()} className="cursor-pointer">
+        {dark ? (
           <AiFillSun size={"25px"}></AiFillSun>
         ) : (
           <AiFillMoon size={"25px"}></AiFillMoon>
