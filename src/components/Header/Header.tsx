032866b39934c2a343e7fd450a7ec94b681ecd33
Caio Lucas <clucaszrtmain@gmail.com>
2026-06-12 01:12:49 -0300

    fix: change the border bottom for the right purple primary

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,7 +7,7 @@ interface HeaderProps {
 
 export default function Header({ dark, changeTheme }: HeaderProps) {
   return (
-    <header className="flex justify-between items-center px-5 py-3 dark:bg-black-base text-purple-primary border-b-primary-purple bg-off-white font-semibold text-xs border-b-2 dark:border-b-off-white duration-300">
+    <header className="flex justify-between items-center px-5 py-3 dark:bg-black-base text-purple-primary border-b-purple-primary bg-off-white font-semibold text-xs border-b-2 dark:border-b-off-white duration-300">
       <p>@lucas-devsss</p>
       <button onClick={() => changeTheme()} className="cursor-pointer">
         {dark ? (
