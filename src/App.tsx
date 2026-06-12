1ae27c42c232cf1349ac9b8cad5060ff007a0bc5
Caio Lucas <clucaszrtmain@gmail.com>
2026-06-12 01:05:15 -0300

    feat: Create a state and a function for darkMode, update the Html for tailwind darkMode and render the Header component with his props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from "react";
+import Header from "./components/Header/Header";
+
 function App() {
+  const [isDark, setIsDark] = useState<boolean>(false);
+
+  useEffect(() => {
+    document.documentElement.classList.toggle("dark");
+  }, [isDark]);
+
+  function changeTheme(): void {
+    setIsDark(!isDark);
+  }
+
   return (
     <>
-      <div className="">
-        <p className="text-purple-primary text-center">Devtree </p>
-      </div>
+      <Header changeTheme={changeTheme} dark={isDark}></Header>
     </>
   );
 }
