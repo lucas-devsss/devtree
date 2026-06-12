aedb392bce3bdb2188cd683e3bb62e2907392d81
Caio Lucas <clucaszrtmain@gmail.com>
2026-06-11 23:24:25 -0300

    add the base files of the project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="">
+        <p className="text-purple-primary text-center">Devtree </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
