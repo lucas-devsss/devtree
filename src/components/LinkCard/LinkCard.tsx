4a54a65524df68fa706e55badc1ba08e95b2e599
Caio Lucas <clucaszrtmain@gmail.com>
2026-06-16 23:01:03 -0300

    feat: Create the LinkCard component

---
+++ src/components/LinkCard/LinkCard.tsx
@@ -0,0 +1,24 @@
+import type { IconType } from "react-icons";
+
+interface LinkCardProps {
+  url: string;
+  name: string;
+  icon: IconType;
+}
+
+export default function LinkCard({ url, name, icon }: LinkCardProps) {
+  const Icon = icon;
+  return (
+    <li className="p-3 flex items-center max-w-100 w-full bg-purple-primary text-off-white rounded-sm border-transparent border-2 hover:border-purple-dark hover:-translate-y-1 duration-300  dark:hover:border-off-white">
+      <Icon size={"50px"} />
+      <a
+        href={url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex-2 text-center font-medium text-base"
+      >
+        {name}
+      </a>
+    </li>
+  );
+}
