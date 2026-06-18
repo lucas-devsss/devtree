e8081e1077c475cab942ccc71f325fbe4ea67720
Caio Lucas <clucaszrtmain@gmail.com>
2026-06-17 21:58:12 -0300

    style: Decrease the padding of linkCards on mobile devices

--- src/components/LinkCard/LinkCard.tsx
+++ src/components/LinkCard/LinkCard.tsx
@@ -9,7 +9,7 @@ interface LinkCardProps {
 export default function LinkCard({ url, name, icon }: LinkCardProps) {
   const Icon = icon;
   return (
-    <li className="p-3 flex items-center max-w-100 w-full bg-purple-primary text-off-white rounded-sm border-transparent border-2 hover:border-purple-dark hover:-translate-y-1 duration-300  dark:hover:border-off-white">
+    <li className="p-2 md:p-3 flex items-center max-w-100 w-full bg-purple-primary text-off-white rounded-sm border-transparent border-2 hover:border-purple-dark hover:-translate-y-1 duration-300  dark:hover:border-off-white">
       <Icon size={"50px"} />
       <a
         href={url}
