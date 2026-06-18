c7c897cdac564e1eb666a10cae21dbdd4f4ad0aa
Caio Lucas <clucaszrtmain@gmail.com>
2026-06-17 23:33:39 -0300

    refactor: Change the paths of images and icons for a short one

--- src/components/ProfileCard/ProfileCard.tsx
+++ src/components/ProfileCard/ProfileCard.tsx
@@ -2,7 +2,7 @@ export default function ProfileCard() {
   return (
     <address className="flex flex-col items-center dark:text-off-white py-2.5 px-7">
       <img
-        src="public/caio_lucas_img.png"
+        src="/caio_lucas_img.png"
         alt=""
         className="w-37.5 h-37.5 rounded-full object-cover object-[50%_30%]"
       />
